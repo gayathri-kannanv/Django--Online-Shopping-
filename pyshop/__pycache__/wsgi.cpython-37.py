3ef2866c5b6a1e1feb085e36073865fcdfc560d8
GAYATHRI K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-05-01 14:22:06 +0530

    Create wsgi.cpython-37.py

---
+++ pyshop/__pycache__/wsgi.cpython-37.py
@@ -0,0 +1,13 @@
+B
+
+    ¨±«^•  ã               @   s0   d Z ddlZddlmZ ej dd¡ eƒ ZdS )zÖ
+WSGI config for pyshop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+é    N)Úget_wsgi_applicationÚDJANGO_SETTINGS_MODULEzpyshop.settings)Ú__doc__ÚosÚdjango.core.wsgir   ÚenvironÚ
+setdefaultÚapplication© r
+   r
+   ú)F:\MY PROJECT WORKS\MYSHOP\pyshop\wsgi.pyÚ<module>   s   
